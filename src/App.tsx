5121b130e87c34ddd75145441a8082082ab3c419
Moksh Sharma <sharmamoksh64@gmail.com>
2024-06-28 13:12:38 +0530

    hlo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DisplayWeather from './componets/DisplayWeather';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayWeather />
+    </div>
+  );
+}
+
+export default App;
